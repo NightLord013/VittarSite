025989cdef7be9305cbebecbd01717950abddd00
NightLord013 <ziganshin914@gmail.com>
2021-05-16 00:05:03 +0300

    Add ajax pages

--- OrderApp/views.py
+++ OrderApp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Order
-from UserApp.models import User
 from .forms import OrderForm
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def home(request):
     """Гланая страница"""
@@ -13,9 +14,26 @@ def user_orders(request):
     """Заказы пользователя"""
     user = request.user
     orders = Order.objects.filter(user=user).order_by('-adding_datetime')
+    paginator = Paginator(orders, 3)
+    page = request.GET.get('page')
+    try:
+        orders = paginator.page(page)
+    except PageNotAnInteger:
+        # Если переданная страница не является числом, возвращаем первую
+        orders =paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # Если получили AJAX - запрос с номером страницы, большим, чем их количество,
+            # возвращаем пустую страницу.
+            return HttpResponse('')
+        # Если номер страницы больше, чем их количество, возвращаем последнюю.
+        orders = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request, 'OrderApp/user_orders_ajax.html', {'orders': orders})
     return render(request, 'OrderApp/user_orders.html', {'orders': orders})
 
 
+@login_required
 def order_form(request):
     """Страница заказа услуги"""
     if request.method == 'POST':
@@ -31,6 +49,22 @@ def order_form(request):
 def all_orders(request):
     """Все заказы пользователей"""
     orders = Order.objects.filter(order_stage='В').order_by('-adding_datetime')
+    paginator = Paginator(orders, 3)
+    page = request.GET.get('page')
+    try:
+        orders = paginator.page(page)
+    except PageNotAnInteger:
+        # Если переданная страница не является числом, возвращаем первую
+        orders = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # Если получили AJAX - запрос с номером страницы, большим, чем их количество,
+            # возвращаем пустую страницу.
+            return HttpResponse('')
+        # Если номер страницы больше, чем их количество, возвращаем последнюю.
+        orders = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request, 'OrderApp/all_orders_ajax.html', {'orders': orders})
     return render(request, 'OrderApp/all_orders.html', {'orders': orders})
 
 @user_passes_test(lambda user: user.is_staff)
